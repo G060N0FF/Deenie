368beaf7c7e4244e826099a0b281dab86f49aded
G060N0FF <georgi.hr.dim@gmail.com>
2020-05-11 14:04:13 +0300

    Update 1

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Lenovo\Documents\Music\venv"
+$env:VIRTUAL_ENV="C:\Users\Lenovo\Documents\Projects\Music\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
